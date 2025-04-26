1d1e0e414c03a3aef304c1e50336d245cd260537
Yaqeen <yaqeenono@gmail.com>
2025-04-26 20:21:58 +0200

    Define NeuralNetwork model architecture

--- classifier.py
+++ classifier.py
@@ -10,3 +10,19 @@ import numpy as np
 # Checks for available device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
+
+
+# Define neural network model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+  
