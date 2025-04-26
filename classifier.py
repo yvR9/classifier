b40a6ec4b659e28e2771e845dd46e4b61a905b6e
Yaqeen <yaqeenono@gmail.com>
2025-04-26 21:19:49 +0200

    ****Define training function used to train model

--- classifier.py
+++ classifier.py
@@ -36,5 +36,16 @@ def train(dataloader, model, loss_fn, optimiser):
     model.train()
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
+      
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
         
-       
+        # Backpropagation
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+        
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
